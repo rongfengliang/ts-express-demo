e5e41918fa3124459c40d0c978719849e8f8a8fa
rongfengliang <1141591465@qq.com>
2019-10-08 14:57:45 +0800

    change ts config

--- dist/UserController.js
+++ dist/UserController.js
@@ -1,6 +1,14 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
 var http_status_codes_1 = require("http-status-codes");
 var core_1 = require("@overnightjs/core");
 var logger_1 = require("@overnightjs/logger");
@@ -19,19 +27,19 @@ var UserController = (function () {
             message: 'post_called',
         });
     };
-    tslib_1.__decorate([
+    __decorate([
         core_1.Get(),
-        tslib_1.__metadata("design:type", Function),
-        tslib_1.__metadata("design:paramtypes", [Object, Object]),
-        tslib_1.__metadata("design:returntype", Object)
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [Object, Object]),
+        __metadata("design:returntype", Object)
     ], UserController.prototype, "get", null);
-    tslib_1.__decorate([
+    __decorate([
         core_1.Post(),
-        tslib_1.__metadata("design:type", Function),
-        tslib_1.__metadata("design:paramtypes", [Object, Object]),
-        tslib_1.__metadata("design:returntype", Object)
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [Object, Object]),
+        __metadata("design:returntype", Object)
     ], UserController.prototype, "post", null);
-    UserController = tslib_1.__decorate([
+    UserController = __decorate([
         core_1.Controller('user/say-hello')
     ], UserController);
     return UserController;
