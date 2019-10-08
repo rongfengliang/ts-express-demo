607210c808b4eeb638746d064bd999491a691de7
rongfengliang <1141591465@qq.com>
2019-10-08 14:33:21 +0800

    add more logs

--- dist/SampleServer.js
+++ dist/SampleServer.js
@@ -1,31 +1,12 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var bodyParser = __importStar(require("body-parser"));
+var tslib_1 = require("tslib");
+var bodyParser = tslib_1.__importStar(require("body-parser"));
 var core_1 = require("@overnightjs/core");
 var logger_1 = require("@overnightjs/logger");
 var UserController_1 = require("./UserController");
 var SampleServer = (function (_super) {
-    __extends(SampleServer, _super);
+    tslib_1.__extends(SampleServer, _super);
     function SampleServer() {
         var _this = _super.call(this, process.env.NODE_ENV === 'development') || this;
         _this.app.use(bodyParser.json());
